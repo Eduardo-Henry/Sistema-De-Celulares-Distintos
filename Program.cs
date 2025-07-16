89e99a0b533cd86f1f83f5812a704356604e3d2a
Eduardo Carvalho <du.h.c.oliveira17@gmail.com>
2025-07-15 21:32:12 -0300

    Agora ambos os modelos de celulares retorna métodos de ligar e receber ligação e retorna que estão baixando algum app

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Teste com a classe Nokia
+        Nokia nokia = new Nokia("179998898", "Nokia C210", "IMEI111111", 32);
+
+        System.Console.WriteLine("===Teste Nokia===\n");
+
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Figma");
+
+        // Teste com a classe Iphone
+        Iphone iphone = new Iphone("11993445", "Iphone 14", "IMEI22222", 256);
+
+        System.Console.WriteLine("===Teste iPhone===\n");
+
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Linkedin");
+    }
+}
